44cec937856592f6bc49fe7f7807c42dcff53dc8
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2026-04-16 16:42:18 +0900

    Initial commit from local

---
+++ src/main/java/com/sang/smite/SmiteApplication.java
@@ -0,0 +1,13 @@
+package com.sang.smite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sang/smite/SmiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sang.smite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
