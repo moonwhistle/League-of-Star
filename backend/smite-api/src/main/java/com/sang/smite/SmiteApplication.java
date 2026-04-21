7fea7d4b2824222e1b5ebaa9c4f7e01f50c92567
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2026-04-21 20:35:44 +0900

    feat: redis repository 스캔 설정 제거

--- backend/smite-api/src/main/java/com/sang/smite/SmiteApplication.java
+++ backend/smite-api/src/main/java/com/sang/smite/SmiteApplication.java
@@ -2,10 +2,11 @@ package com.sang.smite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(exclude = {RedisRepositoriesAutoConfiguration.class})
 public class SmiteApplication {
 
 	public static void main(String[] args) {
