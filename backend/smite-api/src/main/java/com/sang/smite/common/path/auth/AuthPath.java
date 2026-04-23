f43a4c585131bf43711e516900c8fa2e2c415143
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2026-04-23 09:28:01 +0900

    chore: 코드래빗 리뷰 트리거를 위한 푸시

--- backend/smite-api/src/main/java/com/sang/smite/common/path/auth/AuthPath.java
+++ backend/smite-api/src/main/java/com/sang/smite/common/path/auth/AuthPath.java
@@ -5,5 +5,6 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class AuthPath {
+
     public static final String SIGN_UP = "/api/v1/auth/signUp";
 }
